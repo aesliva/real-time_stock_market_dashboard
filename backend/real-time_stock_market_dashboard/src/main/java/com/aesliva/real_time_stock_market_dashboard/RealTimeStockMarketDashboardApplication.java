530c4f655d5b10792ca77b2d003eaefd884982be
Aesli <aesliva0@gmail.com>
2024-08-24 18:18:40 -0500

    init

---
+++ backend/real-time_stock_market_dashboard/src/main/java/com/aesliva/real_time_stock_market_dashboard/RealTimeStockMarketDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.aesliva.real_time_stock_market_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeStockMarketDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeStockMarketDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/real-time_stock_market_dashboard/src/test/java/com/aesliva/real_time_stock_market_dashboard/RealTimeStockMarketDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aesliva.real_time_stock_market_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeStockMarketDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
