543c4ee7cc98d7381b2834c4a1d9203c1333e467
Aesli <aesliva0@gmail.com>
2024-08-25 16:39:18 -0500

    feat: Add IndexRepository.java

---
+++ backend/real-time_stock_market_dashboard/src/main/java/com/aesliva/real_time_stock_market_dashboard/IndexRepository.java
@@ -0,0 +1,7 @@
+package com.aesliva.real_time_stock_market_dashboard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndexRepository extends JpaRepository<Index, Long> {
+
+}
