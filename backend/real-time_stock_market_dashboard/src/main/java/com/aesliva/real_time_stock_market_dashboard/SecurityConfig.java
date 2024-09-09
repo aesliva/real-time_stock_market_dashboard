4b495dbfb9963e4fdbe5acee62504da8a32b2c72
Aesli <aesliva0@gmail.com>
2024-09-09 16:43:56 -0500

    feat: add SecurityConfig.java

---
+++ backend/real-time_stock_market_dashboard/src/main/java/com/aesliva/real_time_stock_market_dashboard/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.aesliva.real_time_stock_market_dashboard;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/indexes").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
