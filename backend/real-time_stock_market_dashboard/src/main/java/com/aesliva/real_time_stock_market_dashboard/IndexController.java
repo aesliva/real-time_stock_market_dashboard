c93ffac5a4f4af0796cbb4da63c759bb4d0cda00
Aesli <aesliva0@gmail.com>
2024-09-09 16:43:09 -0500

    feat: add CrossOrigin annotation in IndexController

--- backend/real-time_stock_market_dashboard/src/main/java/com/aesliva/real_time_stock_market_dashboard/IndexController.java
+++ backend/real-time_stock_market_dashboard/src/main/java/com/aesliva/real_time_stock_market_dashboard/IndexController.java
@@ -1,12 +1,14 @@
 package com.aesliva.real_time_stock_market_dashboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class IndexController {
 
     @Autowired
